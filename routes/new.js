1b9ba8be17ba7001a09a95c78140229db595b67e
Edcreation <93517096+Edcreation@users.noreply.github.com>
2023-01-09 16:52:01 +0200

    comit 2
    error corrected

---
+++ routes/new.js
@@ -0,0 +1,57 @@
+
+const { Router } = require('express');
+const app = Router();
+const Blog = require('../models/blogsModel')
+const { marked } = require('marked');
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/login");
+}
+
+app.get('/dashboard/new', isLoggedIn, (req, res) => {
+    const pop = req.flash('pop-up')
+    res.render('dashboard/new', {pop} )
+})
+
+app.post('/dashboard/new', (req,res) => {
+    function generateRandomWord(length) {
+        const possibleCharacters = 'abcdefghijklmnopqrstuvwxyz';
+        let word = '';
+        for (let i = 0; i < length; i++) {
+          word += possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+        }
+        return word;
+    }
+    const bid = generateRandomWord(12);
+    /*const image = {
+        contentType: req.file.mimetype,
+        image: req.file.buffer
+    };*/
+    const content = marked(req.body.content)
+    const title = req.body.title;
+    const done = "Created!!"
+    const fail = "Failed!!"
+    const blog = new Blog({
+        bid : bid,
+        title : title,
+        content : content,
+    })
+    blog.save(function (err)  {
+        if (err) {
+            console.log(err);
+            req.flash('pop-up', fail);
+            res.redirect('/dashboard/new')
+        } else {
+            req.flash('pop-up', done);
+            res.redirect('/dashboard/new');
+        }
+    })
+})
+
+
+module.exports = app;
